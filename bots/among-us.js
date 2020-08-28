5d102088785e8dabf21afdf19e614675c22a64f0
Brian Quach <briiquach@gmail.com>
2020-08-28 12:19:43 -0400

    Added example env

--- bots/among-us.js
+++ bots/among-us.js
@@ -14,11 +14,27 @@ async function connect() {
     });
     
     client.on('message', msg => {
+      console.log(msg)
       if(!msg.content.startsWith(process.env.IDENTIFIER)) return;
 
-      // Remove identified
+      // Remove identifier
       let command = msg.content.substring((process.env.IDENTIFIER).length).split(' ');
     
+
+      if (['test'].includes(command[0])) {
+        if(!command[1].match(/<@!(\d{18})>/)) return;
+        let id = command[1].substring(3,21);
+
+        msg.member.roles.add("among-us");
+        msg.author.send(process.env.VC_LINK);
+      }
+
+      
+      if (['testundo'].includes(command[0])) {
+        if(!command[1].match(/<@!(\d{18})>/)) return;
+        msg.member.roles.remove("among-us");
+      }
+  
       if (['createQueue', 'cq'].includes(command[0])) {
         queue.createQueue(msg.guild.id, msg.author.id);
       }
